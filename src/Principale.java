5a0e047af25e01d03c50ca9c5e2ad12c0143df81
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-10-17 11:17:09 +0200

    Revert "Fix `==`"
    This reverts commit 4dd3a91a

--- src/Principale.java
+++ src/Principale.java
@@ -1,11 +1,2 @@
-import JDBC.Connect;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class Principale {
-    public static void main(String[] args) throws SQLException {
-        Connection c = Connect.localHost.connect("mesabloo", "root");
-        System.out.println(c);
-    }
 }
