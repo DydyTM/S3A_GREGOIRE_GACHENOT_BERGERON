4acd18c5b8244ef159eea4e6d20c660334c0e5a8
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-10-17 14:00:38 +0200

    Ajout d'un pretty-printer de `ResultSet`

--- src/JDBC/Utils.java
+++ src/JDBC/Utils.java
@@ -1,37 +1,114 @@
 package JDBC;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Utils {
     public static void printQueryResult(ResultSet rs) throws SQLException {
+        HashMap<String, LinkedList<String>> results = new HashMap<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int rowCount = 0;
+
         while (rs.next()) {
-            ResultSetMetaData rsmd = rs.getMetaData();
             for (int i = 1; i <= rsmd.getColumnCount(); ++i) {
-                System.out.print(rsmd.getColumnLabel(i) + ": ");
-                switch (rsmd.getColumnType(i)) {
-                    case Types.VARCHAR:
-                        System.out.print(rs.getString(i) + " ");
-                        break;
-                    case Types.NUMERIC:
-                    case Types.INTEGER:
-                        System.out.print(rs.getInt(i) + " ");
-                        break;
-                    case Types.FLOAT:
-                        System.out.print(rs.getFloat(i) + " ");
-                        break;
-                    case Types.DOUBLE:
-                        System.out.print(rs.getDouble(i) + " ");
-                        break;
-                    default:
-                        System.out.print("Unsupported type " + rsmd.getColumnType(i));
-                }
-                System.out.print("; ");
+                String key = rsmd.getColumnLabel(i);
+                String value = getValueFromMetaDataAt(rs, i);
+                results.computeIfPresent(key, (k, l) -> { l.addLast(value); return l; });
+                results.putIfAbsent(key, new LinkedList<>(Collections.singletonList(value)));
             }
-            System.out.println();
+            rowCount++;
         }
-        System.out.println();
+        String maxColumn = Integer.toString(rowCount);
+
+        if (rowCount == 0) {
+            System.out.println("<<no result>>");
+            return;
+        }
+
+        BiFunction<String, Integer, String> padding = (pad, size) -> {
+            AtomicReference<String> s = new AtomicReference<>("");
+            IntStream.range(0, size).forEach(i -> s.updateAndGet(s_ -> s_ += pad));
+            return s.get();
+        };
+
+        Function<LinkedList<String>, String> longestIn =
+            l -> l.stream().reduce("", (s, s1) -> s.length() >= s1.length() ? s : s1);
+
+        HashMap<String, Integer> paddings = new HashMap<>();
+        results.forEach((k, v) -> paddings.put(k, longestIn.apply(v).length()));
+
+        System.out.print(padding.apply(" ", maxColumn.length()) + " ");
+        paddings.forEach(
+            (k, v) -> System.out.print(
+                "| " + k
+                + padding.apply(" ", v - k.length()) + " ")
+        );
+        System.out.println(" |");
+
+        for (int i = 1; i <= rowCount; ++i) {
+            final int i_ = i;
+            String index = Integer.toString(i);
+
+            System.out.print(index + padding.apply(" ", maxColumn.length() - index.length()) + " ");
+
+            paddings.forEach((k, v) -> {
+                LinkedList<String> vals = results.get(k);
+                String value = vals.get(i_ - 1);
+
+                System.out.print("| " + value
+                    + padding.apply(" ", (v < k.length() ? k.length() : v) - value.length())
+                    + " ");
+            });
+
+            System.out.println(" |");
+        }
+    }
+
+    private static String getValueFromMetaDataAt(ResultSet rs, int index) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        switch (rsmd.getColumnType(index)) {
+            case Types.INTEGER:
+                return Integer.toString(rs.getInt(index));
+            case Types.FLOAT:
+                return Float.toString(rs.getFloat(index));
+            case Types.DOUBLE:
+                return Double.toString(rs.getDouble(index));
+            case Types.NUMERIC: {
+                BigDecimal bd = rs.getBigDecimal(index);
+                return (bd == null) ? "NULL" : bd.toString();
+            }
+            case Types.BOOLEAN:
+                return Boolean.toString(rs.getBoolean(index));
+            case Types.VARCHAR:
+            case Types.CHAR:
+            case Types.LONGVARCHAR: {
+                String s = rs.getString(index);
+                return (s == null) ? "NULL" : "\"" + s + "\"";
+            }
+            case Types.DATE: {
+                Date ts = rs.getDate(index);
+                return (ts == null) ? "NULL" : ts.toString();
+            }
+            case Types.TIME_WITH_TIMEZONE:
+            case Types.TIME: {
+                Time ts = rs.getTime(index);
+                return (ts == null) ? "NULL" : ts.toString();
+            }
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+            case Types.TIMESTAMP: {
+                Timestamp ts = rs.getTimestamp(index);
+                return (ts == null) ? "NULL" : ts.toString();
+            }
+            default:
+                return String.format("[!] %d ???", rsmd.getColumnType(index));
+        }
     }
 }
