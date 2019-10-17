b1c1ee30e02e271ea5f143c2e90d39bc0a806bf9
Raikuji <irub.tech@gmail.com>
2019-10-17 17:33:45 +0200

    Ajout requete 5

--- sql/request.sql
+++ sql/request.sql
@@ -5,3 +5,6 @@ SELECT numplat, libelle from plat group by numplat, libelle having numplat not i
 SELECT nomserv, dataff from serveur natural join affecter where numtab = 15 and dataff between to_date('10/09/2016') and to_date('12/10/2016');
 
 SELECT nomserv, sum(montcom), count(numcom) from commande natural join affecter natural join serveur where dataff between to_date('10/09/2016') and to_date('12/10/2016') group by nomserv;
+
+SELECT nomserv from commande natural join affecter natural join serveur where dataff between to_date('10/09/2016') and to_date('12/10/2016') group by nomserv having sum(montcom) is null;
+-- pb si serveur n'a pas servi : il n'a pas de CA, meme pas egal a null
