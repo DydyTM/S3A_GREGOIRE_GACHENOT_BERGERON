bf83773d4b4cdc3b8fa6cceca41fff01fecad780
Raikuji <irub.tech@gmail.com>
2019-10-17 17:23:34 +0200

    Ajout du fichier avec les requetes sql

---
+++ sql/request.sql
@@ -0,0 +1,7 @@
+SELECT numplat, libelle from plat natural join contient natural join commande where datcom between to_date('10/09/2016') and to_date('12/10/2016') group by numplat, libelle order by numplat;
+
+SELECT numplat, libelle from plat group by numplat, libelle having numplat not in (SELECT numplat from plat natural join contient natural join commande where datcom between to_date('10/09/2016') and to_date('12/09/2016'));
+
+SELECT nomserv, dataff from serveur natural join affecter where numtab = 15 and dataff between to_date('10/09/2016') and to_date('12/10/2016');
+
+SELECT nomserv, sum(montcom), count(numcom) from commande natural join affecter natural join serveur where dataff between to_date('10/09/2016') and to_date('12/10/2016') group by nomserv;
