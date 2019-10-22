f0fd991fbc9afb0c9e296a895a01f6f8fc09b8ab
Raikuji <irub.tech@gmail.com>
2019-10-22 10:58:49 +0200

    Ecran de connection de l'application

--- src/Principale.java
+++ src/Principale.java
@@ -4,10 +4,28 @@ import launcher.Launcher;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Principale {
-    public static void main(String[] args) throws SQLException {
-        Connection c = // Connect.localHost.connect("user", "pass");
+    public static void main(String[] args) {
+
+        boolean connected = false;
+        Connection c = null;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Veuillez vous authentifier");
+        while(!connected) {
+            try {
+                System.out.print("Nom d'utilisateur : ");
+                String user = sc.nextLine();
+                System.out.print("Mot de passe : ");
+                String password = sc.nextLine();
+                c = Connect.univLorraine.connect(user, password);
+                connected = true;
+            } catch (SQLException e) {
+                System.out.println("Erreur d'authentification, veuillez reesayer");
+            }
+        }
         Launcher.from(Requests.class).launch(c);
     }
 }
