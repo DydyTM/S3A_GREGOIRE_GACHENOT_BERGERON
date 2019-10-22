a4b27beeaed2d022996a86968c296297c768d254
Raikuji <irub.tech@gmail.com>
2019-10-22 11:33:56 +0200

    Correction de la quatrieme requete

--- src/request/Request4.java
+++ src/request/Request4.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 
 public class Request4 {
     public static void request(Connection c, String dateD, String dateF) throws SQLException {
-        String req = "SELECT nomserv, sum(montcom), count(numcom) from commande natural join affecter natural join serveur where dataff between to_date('" + dateD + "') and to_date('" + dateF + "') group by nomserv;";
+        String req = "SELECT nomserv, sum(montcom), count(numcom) from commande natural join affecter natural join serveur where dataff between to_date('" + dateD + "') and to_date('" + dateF + "') group by nomserv";
         Statement stmt = c.createStatement();
         ResultSet rs = stmt.executeQuery(req);
         Utils.printQueryResult(rs);
