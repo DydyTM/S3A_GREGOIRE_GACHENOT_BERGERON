02c0804b9053ea89d1846f0fe4df522702f5454d
Raikuji <irub.tech@gmail.com>
2019-10-22 13:27:36 +0200

    Modif req1

--- src/request/Request1.java
+++ src/request/Request1.java
@@ -12,6 +12,8 @@ public class Request1 {
         String req = "SELECT numplat, libelle from plat natural join contient natural join commande where datcom between to_date('" + dateD + "') and to_date('" + dateF + "') group by numplat, libelle order by numplat";
         Statement stmt = c.createStatement();
         ResultSet rs = stmt.executeQuery(req);
+        System.out.println("\nRequesting ...\n");
         Utils.printQueryResult(rs);
+        System.out.print("\n\n");
     }
 }
