8a511945ec5d1cc1c019116e84711789db9228dd
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-10-22 10:12:30 +0200

    Ajout de la classe de requêtes

---
+++ src/JDBC/Requests.java
@@ -0,0 +1,12 @@
+package JDBC;
+
+import launcher.LauncherEntry;
+
+import java.sql.Connection;
+
+public class Requests {
+    @LauncherEntry
+    public void Test(Connection c) {
+
+    }
+}
--- src/Principale.java
+++ src/Principale.java
@@ -1,11 +1,13 @@
 import JDBC.Connect;
+import JDBC.Requests;
 import launcher.Launcher;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Principale {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection c = // Connect.localHost.connect("user", "pass");
-        Launcher.from(/* Requests.class */).launch(c);
+        Launcher.from(Requests.class).launch(c);
     }
 }
