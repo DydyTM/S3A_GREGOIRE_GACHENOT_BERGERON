5fc734a0b9c99628284e3134b7e98694dadf84d4
Raikuji <irub.tech@gmail.com>
2019-10-22 10:46:01 +0200

    Ajout requete 6

--- sql/request.sql
+++ sql/request.sql
@@ -7,3 +7,5 @@ SELECT nomserv, dataff from serveur natural join affecter where numtab = 15 and 
 SELECT nomserv, sum(montcom), count(numcom) from commande natural join affecter natural join serveur where dataff between to_date('10/09/2016') and to_date('12/10/2016') group by nomserv;
 
 SELECT nomserv from serveur group by nomserv having nomserv not in (SELECT nomserv from commande natural join affecter natural join serveur where dataff between to_date('10/09/2016') and to_date('12/10/2016'));
+
+update COMMANDE set montcom = (select sum(prixunit * quantite) from CONTIENT natural join PLAT where NUMCOM = 100) where numcom = 100;
