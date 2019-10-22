d440d1fa5a975018c8a7a9a2fb9523e8bc80eecd
Raikuji <irub.tech@gmail.com>
2019-10-22 11:20:02 +0200

    Creation de la premiere requete

---
+++ src/request/Request1.java
@@ -0,0 +1,17 @@
+package request;
+
+import JDBC.Utils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Request1 {
+    public static void request(Connection c, String dateD, String dateF) throws SQLException {
+        String req = "SELECT numplat, libelle from plat natural join contient natural join commande where datcom between to_date('" + dateD + "') and to_date('" + dateF + "') group by numplat, libelle order by numplat";
+        Statement stmt = c.createStatement();
+        ResultSet rs = stmt.executeQuery(req);
+        Utils.printQueryResult(rs);
+    }
+}
