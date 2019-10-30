22f82a34eab1fba064c52f0d6a744f5af8626d0c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-10-30 17:20:20 +0100

    Ajout handling des requêtes

--- src/JDBC/Requests.java
+++ src/JDBC/Requests.java
@@ -2,16 +2,52 @@ package JDBC;
 
 import launcher.LauncherEntry;
 import launcher.Param;
-import request.Request1;
+import request.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Requests {
-    @LauncherEntry(order=1)
-    public void Req_1(Connection c, @Param(name="date_debut(dd/mm/yyyy)") Object date1, @Param(name="date_fin(dd/mm/yyyy)") Object date2) {
+    @LauncherEntry
+    public void Req1(Connection c, @Param(name="date_debut(\"dd/mm/yyyy\")") Object date1, @Param(name="date_fin(\"dd/mm/yyyy\")") Object date2) {
         try {
             Request1.request(c, (String) date1, (String) date2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @LauncherEntry
+    public void Req2(Connection c, @Param(name="date_debut(\"dd/mm/yyyy\")") Object date1, @Param(name="date_fin(\"dd/mm/yyyy\")") Object date2) {
+        try {
+            Request2.request(c, (String) date1, (String) date2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @LauncherEntry
+    public void Req3(Connection c, @Param(name="num_tab") Object numtab, @Param(name="date_debut(\"dd/mm/yyyy\")") Object date1, @Param(name="date_fin(\"dd/mm/yyyy\")") Object date2) {
+        try {
+            Request3.request(c, (Integer) numtab, (String) date1, (String) date2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @LauncherEntry
+    public void Req4(Connection c, @Param(name="date_debut(\"dd/mm/yyyy\")") Object date1, @Param(name="date_fin(\"dd/mm/yyyy\")") Object date2) {
+        try {
+            Request4.request(c, (String) date1, (String) date2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @LauncherEntry
+    public void Req5(Connection c, @Param(name="date_debut(\"dd/mm/yyyy\")") Object date1, @Param(name="date_fin(\"dd/mm/yyyy\")") Object date2) {
+        try {
+            Request5.request(c, (String) date1, (String) date2);
         } catch (SQLException e) {
             e.printStackTrace();
         }
